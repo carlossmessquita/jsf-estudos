037e23b2d38f3a96b3e00eab5d9e6e05c2fb16b7
carlossmsqt <carlossmessquita@gmail.com>
2023-02-26 21:11:17 -0300

    Primeira Versão Configurada

---
+++ target/classes/META-INF/sql/dados-iniciais.sql
@@ -0,0 +1,11 @@
+insert into ramo_atividade (id, descricao) values (1, 'Distribuição de Alimentos');
+insert into ramo_atividade (id, descricao) values (2, 'Telecomunicações');
+insert into ramo_atividade (id, descricao) values (3, 'Comércio Eletrônico');
+insert into ramo_atividade (id, descricao) values (4, 'Turismo');
+insert into ramo_atividade (id, descricao) values (5, 'Bar e Restaurante');
+
+insert into empresa (id, cnpj, nome_fantasia, razao_social, tipo, data_fundacao, ramo_atividade_id) values (1, '70.183.193/0001-87', 'Extramercantil', 'Extramercantil LTDA', 'LTDA', '2009-02-02', 1);
+insert into empresa (id, cnpj, nome_fantasia, razao_social, tipo, data_fundacao, ramo_atividade_id) values (2, '60.178.987/0002-89', 'Falamuito Telecom', 'Falamuito Telecom SA', 'SA', '1989-03-21', 2);
+insert into empresa (id, cnpj, nome_fantasia, razao_social, tipo, data_fundacao, ramo_atividade_id) values (3, '12.456.789/0001-56', 'Weekend', 'This.envolve MEI', 'MEI', '2022-03-01', 3);
+insert into empresa (id, cnpj, nome_fantasia, razao_social, tipo, data_fundacao, ramo_atividade_id) values (4, '87.789.654/0002-59', 'Acopla', 'AcoplaFood LTDA', 'LTDA', '1999-08-25', 4);
+insert into empresa (id, cnpj, nome_fantasia, razao_social, tipo, data_fundacao, ramo_atividade_id) values (5, '89.298.478/0001-78', 'Balacobaco', 'Balacobaco RSTR EIRELLI', 'EIRELLI', '2001-05-06', 5);
